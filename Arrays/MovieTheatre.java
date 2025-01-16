b5d9348212e2ad9c5d4c6ff62295a6e1c703092f
Logan Oram <logan.oram@keyin.com>
2025-01-16 15:01:00 -0330

    movieTheatre

--- Arrays/MovieTheatre.java
+++ Arrays/MovieTheatre.java
@@ -1,4 +1,10 @@
+//Simulate a movie theater that helps users to book and reserve seats.
+//1. Reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+//2. Cancel seats
+//3. Retrieve initial seating charting
+
 package Arrays;
 
 public class MovieTheatre {
+
 }
